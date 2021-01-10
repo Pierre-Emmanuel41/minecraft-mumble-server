32db2dcff6e6a2f29ac5891054b182bcf356765c
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-01-10 15:56:03 +0100

    Adding file plugin.yml and classes EventListener and MumbleServerPlugin

---
+++ src/main/java/fr/pederobien/minecraftmumbleserver/EventListener.java
@@ -0,0 +1,56 @@
+package fr.pederobien.minecraftmumbleserver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import fr.pederobien.mumble.server.interfaces.IMumbleServer;
+import fr.pederobien.mumble.server.interfaces.IPlayer;
+
+public class EventListener implements Listener {
+	private IMumbleServer mumbleServer;
+	private Map<Player, IPlayer> players;
+
+	public EventListener(IMumbleServer mumbleServer) {
+		this.mumbleServer = mumbleServer;
+		players = new HashMap<Player, IPlayer>();
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onPlayerJoinEvent(PlayerJoinEvent event) {
+		players.put(event.getPlayer(), mumbleServer.addPlayer(event.getPlayer().getAddress(), event.getPlayer().getName(), event.getPlayer().isOp()));
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onPlayerQuiEvent(PlayerQuitEvent event) {
+		players.remove(event.getPlayer());
+		mumbleServer.removePlayer(event.getPlayer().getName());
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onPlayerCommandPreprocessEvent(PlayerCommandPreprocessEvent event) {
+		String[] command = event.getMessage().substring(1).split(" ");
+		if (command.length == 0)
+			return;
+
+		if (command[0].equals("op")) {
+			Optional<Map.Entry<Player, IPlayer>> optEntry = players.entrySet().stream().filter(entry -> entry.getKey().getName().equals(command[1])).findFirst();
+			if (optEntry.isPresent())
+				optEntry.get().getValue().setAdmin(true);
+		}
+
+		if (command[0].equals("deop")) {
+			Optional<Map.Entry<Player, IPlayer>> optEntry = players.entrySet().stream().filter(entry -> entry.getKey().getName().equals(command[1])).findFirst();
+			if (optEntry.isPresent())
+				optEntry.get().getValue().setAdmin(false);
+		}
+	}
+}
---
+++ src/main/java/fr/pederobien/minecraftmumbleserver/MumbleServerPlugin.java
@@ -0,0 +1,34 @@
+package fr.pederobien.minecraftmumbleserver;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.pederobien.mumble.server.impl.MumbleServer;
+import fr.pederobien.mumble.server.interfaces.IMumbleServer;
+
+public class MumbleServerPlugin extends JavaPlugin {
+	private IMumbleServer server;
+
+	@Override
+	public void onEnable() {
+		try {
+			server = new MumbleServer("Mumble-1.0-SNAPSHOT", InetAddress.getByName(Bukkit.getIp()), 28000, 32000);
+			server.open();
+			getServer().getPluginManager().registerEvents(new EventListener(server), this);
+
+			for (Player player : Bukkit.getOnlinePlayers())
+				server.addPlayer(player.getAddress(), player.getName(), player.isOp());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		server.close();
+	}
+}
